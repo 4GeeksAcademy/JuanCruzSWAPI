197b2c26c17a3bd4ae235804f871c71b33c9066f
Juan Cruz Pandolfo Martinez <juancruzpandolfo@gmail.com>
2025-02-12 07:35:22 +0000

    baby yoda replacement

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,6 +3,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import babyYodaImage from "../../img/baby.yoda.jpg";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -13,7 +14,6 @@ export const Home = () => {
     actions.getPeople();
   }, []);
 
-  
   const openModal = (content) => {
     setModalContent(content);
     setIsModalOpen(true);
@@ -26,34 +26,31 @@ export const Home = () => {
 
   return (
     <div className="allCards-wrapper">
-    <div className="allCards">
-      {store.people.map((character) => {
-        return (
-          <div
-            className="card bg-dark"
-            key={character.uid}
-            style={{ width: "13rem" }}
-          >
-            <img
-              src={`https://starwars-visualguide.com/assets/img/characters/${character.uid}.jpg`}
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <h5 className="card-title">{character.name}</h5>
-              <ul className="cardlist"></ul>
-              <Link to={`/character/${character.uid}`}>
-        <span className="navbar-brand mb-0 h1">Info</span>
-      </Link>
+      <div className="allCards">
+        {store.people.map((character) => {
+          return (
+            <div
+              className="card bg-dark"
+              key={character.uid}
+              style={{ width: "13rem" }}
+            >
+              <img
+                //src={`https://starwars-visualguide.com/assets/img/characters/${character.uid}.jpg`}
+                src={babyYodaImage}
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">{character.name}</h5>
+                <ul className="cardlist"></ul>
+                <Link to={`/character/${character.uid}`}>
+                  <span className="navbar-brand mb-0 h1">Info</span>
+                </Link>
+              </div>
             </div>
-          </div>
-          
           );
-      })}
-
-      
-      
+        })}
+      </div>
     </div>
-    </div>);
-
+  );
 };
